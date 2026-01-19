62d0bed6d90b31a17829c0e6db1f7349c74d756f
mattsoh <matthew@youthacks.org>
2026-01-19 17:41:24 +0000

    fixed test again

--- backend/test/mailers/user_mailer_test.rb
+++ backend/test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.with(email: "to@example.org", code: "123456", expires_in: 10).verification_code
     assert_equal "Youthacks Auth Verification Code - 123456", mail.subject
     assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
+    assert_equal [ "auth@youthacks.org" ], mail.from
     assert_match "Hi", mail.body.encoded
   end
 end
