19bacc286f7d7158b96e85c0099726e93e16f11c
Matthew Soh <matthew@mattsoh.dev>
2026-02-01 17:57:20 +0000

    oops forgot to migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_01_090000) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_01_091500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_01_090000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.jsonb "payload", default: {}, null: false
+    t.integer "failed_attempts", default: 0, null: false
   end
 
   create_table "refresh_tokens", force: :cascade do |t|
