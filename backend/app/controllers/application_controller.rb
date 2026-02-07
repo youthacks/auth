dde011500dc42aa615914dbccc0437bc6c629bc6
Matthew Soh <matthew@mattsoh.dev>
2026-02-07 12:26:48 +0000

    Revert "don't need this either - fresh start!"
    This reverts commit f29a73eaed88e0407d9b5dbd2f7b8e78cfb7c1bf.
    
    Ok maybe I needed this

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+	include ActionController::Cookies
+
+	private
+
+	def issue_idp_session(user)
+		payload = {
+			sub: user.id,
+			exp: 1.day.from_now.to_i,
+			iat: Time.current.to_i,
+			type: "idp"
+		}
+
+		JWT.encode(payload, Rails.application.secret_key_base, "HS256")
+	end
+
+	def current_idp_user
+		token = cookies.encrypted[:idp_session].to_s
+		return nil if token.blank?
+
+		decoded, = JWT.decode(token, Rails.application.secret_key_base, true, { algorithm: "HS256" })
+		return nil unless decoded["type"] == "idp"
+
+		User.find_by(id: decoded["sub"])
+	rescue JWT::DecodeError
+		nil
+	end
 end
