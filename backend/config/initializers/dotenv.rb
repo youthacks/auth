25e5d458733cf799a6806b02883e42587f00b8cc
Matthew Soh <matthew@mattsoh.dev>
2026-02-08 15:35:11 +0000

    test fix to add .env.production

--- backend/config/initializers/dotenv.rb
+++ backend/config/initializers/dotenv.rb
@@ -1,3 +1,6 @@
 if defined?(Dotenv)
-  Dotenv.load(Rails.root.join(".env"))
+  env_file = ".env"
+  env_file = ".env.production" if Rails.env.production?
+
+  Dotenv.load(Rails.root.join(env_file))
 end
