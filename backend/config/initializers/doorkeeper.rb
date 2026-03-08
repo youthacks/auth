76527f08801de487cc63796e054c065e2c89869a
Matthew Soh <matthew@mattsoh.dev>
2026-03-08 22:35:22 +0000

    update scopes

--- backend/config/initializers/doorkeeper.rb
+++ backend/config/initializers/doorkeeper.rb
@@ -249,8 +249,7 @@ Doorkeeper.configure do
   # For more information go to
   # https://doorkeeper.gitbook.io/guides/ruby-on-rails/scopes
   #
-  default_scopes :openid, :profile
-  optional_scopes :email
+  default_scopes :openid, :profile, :email, :name
 
   # Allows to restrict only certain scopes for grant_type.
   # By default, all the scopes will be available for all the grant types.
